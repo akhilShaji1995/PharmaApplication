9ff11294dd9934a40262abc19f194424b6c9a6d1
Akhil Shaji <akhil.shaji@waiin.com>
2024-06-27 13:00:51 +0530

    Setting Up React and React Router

---
+++ pharma-app/src/components/ProductDetails.js
@@ -0,0 +1,13 @@
+// src/components/ProductDetails.js
+import React from 'react';
+
+const ProductDetails = () => {
+  return (
+    <div>
+      <h1>Product Details</h1>
+      <p>Here you can find details of our pharmaceutical products.</p>
+    </div>
+  );
+};
+
+export default ProductDetails;
